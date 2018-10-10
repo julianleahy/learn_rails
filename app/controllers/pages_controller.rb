22c66c23288a2cf07933c2543ffcd17f7595d67f
julianleahy <julianleahynz@gmail.com>
2018-10-11 12:56:04 +1300

    add route for home and about page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ActionController::Base
+
+    # will look for app/views/pages/home.html.erb
+    def home 
+    end
+
+    # app/views/pages/about.html.erb
+    def about
+    end
+    
+end
